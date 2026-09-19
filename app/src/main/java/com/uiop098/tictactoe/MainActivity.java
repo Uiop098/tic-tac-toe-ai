2110e82287ee338b00d0f993ee0376a5ee4f4dca
Uiop098 <cocfan09876@gmail.com>
2026-09-19 09:54:22 +0530

    fix: resolve crash on startup by removing AppCompat requirement for Default Theme

--- app/src/main/java/com/uiop098/tictactoe/MainActivity.java
+++ app/src/main/java/com/uiop098/tictactoe/MainActivity.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends android.app.Activity {
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
